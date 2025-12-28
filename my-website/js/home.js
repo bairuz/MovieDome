e39bde3ac5cfe9933a9b60f9ca535e44701206f2
bairuz <janibravo11@gmail.com>
2025-12-28 23:18:08 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -64,6 +64,8 @@ const API_KEY = 'dfd8d0c0e19038ba3059fc9043e64c68';
         embedURL = `https://vidsrc.net/embed/${type}/?tmdb=${currentItem.id}`;
       } else if (server === "player.videasy.net") {
         embedURL = `https://player.videasy.net/${type}/${currentItem.id}`;
+      }else if (server === "111movies.com") {
+        embedURL = `https://111movies.com/${type}/${currentItem.id}`;
       }
 
       document.getElementById('modal-video').src = embedURL;
@@ -122,3 +124,4 @@ const API_KEY = 'dfd8d0c0e19038ba3059fc9043e64c68';
 
 
     init();
+
